7969ff9b70e138b92706ffe6f61ec3bd85ad1a8a
nayokgod <pratimakorntippanya@outlook.com>
2024-03-02 19:35:50 +0700

    .

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,8 +8,8 @@ var bodyParser = require('body-parser');
 const path = require("path");
 
 // Base URL for the API
-const base_url = 'http://localhost:3000';
-// const base_url = 'http://node58404-env-9740325.proen.app.ruk-com.cloud';
+// const base_url = 'http://localhost:3000';
+const base_url = 'http://node58404-env-9740325.proen.app.ruk-com.cloud';
 
 // Set the template engine
 app.set("views", path.join(__dirname, "/public/views"))
